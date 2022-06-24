8c1374c3452aca505bfe28129933b29d94865d0e
Tom Davison <thomas.davison@thinkmoney.co.uk>
2022-06-24 09:33:45 +0100

    Adding some grundy code

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func main() {
 
+	// identical expression in logical comparitor
+	// empty branch
+	if 1 == 1 {
+
+	}
+	//duplicate code
+	if 1 == 1 {
+
+	}
+
+	//error not handled
+	json, _ := json.Marshal("marshal_me")
+	//Output not used
+	fmt.Sprint(json)
+
+	//loop condition never changes
 	for i := 0; i < 1; i++ {
 		panic("I don't like loops")
 	}
